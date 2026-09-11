5b223e0d2721298ea51d409d5e334dbacb62cbde
hamaprod <110353198+hamabeads@users.noreply.github.com>
2026-09-11 13:12:46 -0300

    Update albums.js

--- js/albums.js
+++ js/albums.js
@@ -53,7 +53,7 @@ window.ALBUM_LIBRARY = [
 
     id: "CRB",
     title: "CRB",
-    artist: "CRB",
+    artist: "O Maior de Alagoas",
     cover: "albums/CRB/capa.png",
     tracks: [
       { title: "Hino do CRB", file: "albums/CRB/Hino do CRB.mp3" },
